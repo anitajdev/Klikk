7835e1e2bd529a2186d0c9a02af94429d085c5c7
Anita <jelicanita7@gmail.com>
2024-07-06 20:02:13 +0200

    Search button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,21 +11,43 @@ const Navbar = () => {
            <Link href="/" className="font-bold text-xl text-blue-600">Klikk</Link>
         </div>
         {/* CENTER */}
-        <div className="hidden md:flex w-[50%] text-sm">
+        <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
             {/* LINKS */}
             <div className="flex gap-6 text-gray-600">
                 <Link href="/" className="flex items-center gap-2 ">
-                    <Image src="/home.png" alt="Homepage" width={16} height={16} className="w-4 h-4"/>
+                    <Image 
+                        src="/home.png" 
+                        alt="Homepage" 
+                        width={16} 
+                        height={16} 
+                        className="w-4 h-4"
+                    />
                     <span>Homepage</span>
                 </Link>
                 <Link href="/" className="flex items-center gap-2 ">
-                    <Image src="/friends.png" alt="Friends" width={16} height={16} className="w-4 h-4"/>
+                    <Image 
+                        src="/friends.png" 
+                        alt="Friends" 
+                        width={16} 
+                        height={16} 
+                        className="w-4 h-4"
+                    />
                     <span>Friends</span>
                 </Link>
                 <Link href="/" className="flex items-center gap-2 ">
-                    <Image src="/stories.png" alt="Stories" width={16} height={16} className="w-4 h-4"/>
+                    <Image 
+                        src="/stories.png" 
+                        alt="Stories" 
+                        width={16} 
+                        height={16} 
+                        className="w-4 h-4"
+                    />
                     <span>Stories</span>
                 </Link>
+            </div>
+            <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+                <input type="text" placeholder="search..." className="bg-transparent outline-none" />
+                <Image src="/search.png" alt="" width={14} height={14} />
             </div>
         </div>
         {/* RIGHT */}
@@ -36,7 +58,7 @@ const Navbar = () => {
             <ClerkLoaded>
                 <SignedIn>
                     <div className="cursor-pointer">
-                        <Image src="/people.png" alt="" width={20} height={20} />
+                        <Image src="/people.png" alt="" width={24} height={24} />
                     </div>
                     <div className="cursor-pointer">
                         <Image src="/messages.png" alt="" width={20} height={20} />
