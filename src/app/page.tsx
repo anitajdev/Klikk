a34800afba87fecef47629e9d680e2a7bac8d97a
Anita <jelicanita7@gmail.com>
2024-07-09 23:08:32 +0200

    Right bar (Menu) design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ import Stories from "@/components/Stories"
 const Homepage = () => {
   return (
     <div className="flex gap-6 pt-6">
-      <div className="hidden xl:block w-[20%]"><LeftMenu /></div>
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu />
+      </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6 pt-6">
           <Stories />
@@ -15,8 +17,9 @@ const Homepage = () => {
           <Feed />
         </div>
       </div>
-      <div className="hidden lg:block w-[30%]"><RightMenu /></div>
-
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu />
+      </div>
     </div>
   )
 }
--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,6 +1,21 @@
-const RightMenu = () => {
+import Ad from "./Ad"
+import Birthdays from "./Birthdays"
+import FriendRequest from "./FriendRequest"
+import UserInfoCard from "./UserInfoCard"
+import UserMediaCard from "./UserMediaCard"
+
+const RightMenu = ({ userId }: { userId?: string }) => {
   return (
-    <div>RightMenu</div>
+    <div className="flex flex-col gap-6">
+      {userId ? (<>
+      <UserInfoCard userId={userId} />
+      <UserMediaCard userId={userId}/>
+      
+      </>) : null }
+      <FriendRequest />
+      <Birthdays />
+      <Ad size="md"/>
+    </div>
   )
 }
 
