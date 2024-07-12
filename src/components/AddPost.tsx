f9718d2eeb5649bc3a381a20a537e62eb32c9c55
Anita <jelicanita7@gmail.com>
2024-07-12 23:49:40 +0200

    Prisma CRUD

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,6 +1,23 @@
+import prisma from "@/lib/client";
 import Image from "next/image"
 
 const AddPost = () => {
+
+  const testAction = async (formData:FormData) => {
+    // const desc = formData.get("desc") as string;
+    // try {
+    //   prisma.post.create({
+    //     data: {
+    //       userId:
+    //       desc: desc,
+    //     }
+    //   })
+
+    // } catch (err) {
+    //   console.log(err);
+    // }
+  }
+
   return (
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* AVATAR */}
@@ -14,8 +31,12 @@ const AddPost = () => {
       {/* POST */}
       <div className="flex-1">
         {/* TEXT INPUT */}
-        <div className="flex gap-4">
-          <textarea placeholder="What's on your mind?" className="flex-1 bg-slate-100 rounded-lg p-2"></textarea>
+        <form action={testAction} className="flex gap-4">
+          <textarea 
+            placeholder="What's on your mind?" 
+            className="flex-1 bg-slate-100 rounded-lg p-2"
+            name="desc"
+          ></textarea>
           <Image 
             src="/emoji.png"
             alt="" 
@@ -23,7 +44,8 @@ const AddPost = () => {
             height={20} 
             className="w-5 h-12 cursor-pointer self-end"
           />
-        </div>
+          <button>Send</button>
+        </form>
         {/* POST OPTIONS */}
         <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
           <div className="flex items-center gap-2 cursor-pointer">
