9595a13bf3462dab186e16fb5e5691b76585cee8
Anita <jelicanita7@gmail.com>
2024-07-16 23:58:17 +0200

    Update actions.ts

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,2 +1,23 @@
 "use server"
 
+import { auth } from "@clerk/nextjs/server"
+import prisma from "./client"
+
+export const switchFollow = async (userId:string) => {
+    const {userId: currentUserId} = auth()
+
+    try {
+
+        const existingFollow = await prisma.follower.findFirst({
+            where:{
+                followerId:currentUserId,
+                following: userId,
+            }
+        })
+        
+    } catch (err) {
+        console.log(error)
+        throw new Error("Something went wrong")
+    }
+
+}
